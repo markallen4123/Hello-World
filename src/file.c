5629d60bf1d1456dc6f5cda1b80473bf7944c1bc
Mark A <mark@Sophia-14.0-Develpoment>
2014-12-01 13:58:55 -0500

    Adding a dev branch to Hello-World with some changes #3

--- src/file.c
+++ src/file.c
@@ -1,3 +1,5 @@
 This is a new file
 
 Added a new line
+
+Adding a new line to this file in the dev branch
---
+++ src/file4.c
@@ -0,0 +1,7 @@
+# This is a new file being submitted in the dev branch
+qqq
+www
+eee
+rrr
+ttt
+
